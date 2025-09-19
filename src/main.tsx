e640b21bb2c4b5b87c0a0f7073432dfe20938f8b
brian <longth.bka@gmail.com>
2025-09-19 21:26:33 +0700

    fix main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,13 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import { Layout, Home } from "./App";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
     errorElement: <NotFound />,
     children: [
       {
